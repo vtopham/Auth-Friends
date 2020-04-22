adf2ab0eba8a03864c04171d7130f473aa6eab0f
Victoria  Topham <topham.victoria@gmail.com>
2020-04-21 18:07:39 -0700

    styled the nav

--- friends/src/App.js
+++ friends/src/App.js
@@ -5,26 +5,62 @@ import PrivateRoute from './components/PrivateRoute'
 import Friends from './components/Friends'
 import Login from './components/Login'
 
+import styled from 'styled-components'
+
+const AppDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  nav {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    text-decoration: none;
+    background: #f8bdff;
+
+    span {
+      margin: 2%;
+      padding: 1% 2%;
+      border-radius: 5px;
+      background: #85beff;
+      // border: 1px solid #f8bdff;
+      
+
+      &:hover {
+        font-weight: bold;
+      }
+      
+      * {
+        text-decoration: none;
+        color: white;// 
+        
+        
+      }
+    }
+  }
+
+`
 
 function App() {
   return (
     <>
     <Router>
-      <div className = "App">
-        <ul>
-          <li>
+      <AppDiv>
+        <nav>
+          <span>
             <Link to = "/login">Login</Link>
-          </li>
-          <li>
+          </span>
+          <span>
             <Link to = "/friends">Friends</Link>
-          </li>
-        </ul>
+          </span>
+        </nav>
         <Switch>
           <PrivateRoute exact path = "/friends" component = {Friends} />
           <Route path = "/login" component = {Login} />
           <Route component = {Login} />
         </Switch>
-      </div>
+      </AppDiv>
     </Router>
     </>
   );
