bdf2b60025e12c521426b76d146d8c4397a28323
Victoria  Topham <topham.victoria@gmail.com>
2020-04-21 18:34:17 -0700

    styled the NewFriend component

--- friends/src/App.js
+++ friends/src/App.js
@@ -17,14 +17,14 @@ const AppDiv = styled.div`
     display: flex;
     justify-content: flex-end;
     text-decoration: none;
-    background: #f8bdff;
+    background: #f8bdff; 
 
     span {
       margin: 2%;
       padding: 1% 2%;
       border-radius: 5px;
-      background: #85beff;
-      // border: 1px solid #f8bdff;
+      background: #85beff; //this is the blue
+      // border: 1px solid #f8bdff; //this is the pink
       
 
       &:hover {
--- friends/src/components/NewFriend.js
+++ friends/src/components/NewFriend.js
@@ -1,6 +1,77 @@
 import React from 'react'
 import { axiosWithAuth } from '../utils/axiosWithAuth'
+import styled from 'styled-components'
 
+
+const NewFriendDiv = styled.div`
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    * {
+        width: 45%;
+    }
+    
+    width: 100%;
+    height: 300px;
+    margin: 2%;
+    padding: 1%;
+    
+    background-color: #ffffff;
+    background-image: url("/assets/smoke.jpg");
+    background-repeat: no-repeat, repeat;
+    background-size: 100%;
+
+    color: white;
+    // color: black;
+    h2 {
+        font-size: 48px;
+        margin-left: 10%;
+    }
+
+    form {
+        .new-input {
+            margin: 2% 0;
+            padding: 1% 0;
+
+            display: flex;
+            justify-content: space-between;
+
+            label {
+                margin-right: 5%;
+            }
+
+            input {
+                border: none;
+                width: 80px;
+                
+            }
+        }
+
+        button {
+            background: none;
+            padding: 2% 1%;
+            width: 150px;
+            height: 30px;
+            font-size: 14px;
+            border-radius: 5px;
+            border: 2px solid white;
+            margin-top: 5%;
+            color: white;
+            font-weight: bold;
+
+            &:hover {
+                color: #85beff;
+                border: 2px solid #85beff;
+            }
+            
+        }
+    }
+
+
+
+
+`
 //This is our form for creating a new friend
 class NewFriend extends React.Component{
     //state mirrors what is needed in the api call
@@ -37,28 +108,30 @@ class NewFriend extends React.Component{
     //render our form to the dom
     render() {
         return (
-            <>
+            <NewFriendDiv>
                 <h2>Add A New Friend</h2>
-                <div className = "new-input">
-                    <label htmlFor = "name">Name: </label>
-                    <input onChange = {this.handleChange} type = "text" name = "name" value = {this.state.name}/>
-                </div>
-                <div className = "new-input">
-                    <label htmlFor = "age">Age: </label>
-                    <input onChange = {this.handleChange} type = "text" name = "age" value = {this.state.age}/>
-                </div>
-                <div className = "new-input">
-                    <label htmlFor = "email">Email: </label>
-                    <input onChange = {this.handleChange} type = "text" name = "email" value = {this.state.email}/>
-                </div>
-                <div className = "new-input">
-                    <label htmlFor = "doyoulikethem">Liked? </label>
-                    <input onChange = {this.handleChange} type = "text" name = "doyoulikethem" value = {this.state.doyoulikethem}/>
-                </div>
-                <div className = "new-friend-button">
-                    <button onClick = {this.addFriend}>Add Friend</button>
-                </div>
-            </>
+                <form>
+                    <div className = "new-input">
+                        <label htmlFor = "name">Name: </label>
+                        <input onChange = {this.handleChange} type = "text" name = "name" value = {this.state.name}/>
+                    </div>
+                    <div className = "new-input">
+                        <label htmlFor = "age">Age: </label>
+                        <input onChange = {this.handleChange} type = "text" name = "age" value = {this.state.age}/>
+                    </div>
+                    <div className = "new-input">
+                        <label htmlFor = "email">Email: </label>
+                        <input onChange = {this.handleChange} type = "text" name = "email" value = {this.state.email}/>
+                    </div>
+                    <div className = "new-input last-input">
+                        <label htmlFor = "doyoulikethem">Liked? </label>
+                        <input onChange = {this.handleChange} type = "text" name = "doyoulikethem" value = {this.state.doyoulikethem}/>
+                    </div>
+                    <div className = "new-friend-button">
+                        <button onClick = {this.addFriend}>Add Friend</button>
+                    </div>
+                </form>
+            </NewFriendDiv>
         )
     }
 }
